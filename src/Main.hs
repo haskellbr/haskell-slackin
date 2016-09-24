1635d6f3cd74c98bf7b6a70263e3b909bd59e7e4
yamadapc <tacla.yamada@gmail.com>
2016-09-24 15:06:32 -0300

    Serve a homepage #4

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,25 @@
-main = putStrLn "Hello World!"
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TemplateHaskell       #-}
+import           Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App where
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $
+    [whamlet|
+<h1>
+  Haskell Slackin
+    |]
+
+main :: IO ()
+main = warp 3000 App
